e29e69c17ea41abaa4bed92304b8567e53191791
madhatter1999 <anthonyhealy90@gmail.com>
2023-11-13 15:03:26 -0400

    added a change

--- src/components/ChessGame.tsx
+++ src/components/ChessGame.tsx
@@ -3,40 +3,60 @@ import Chessboard from 'chessboardjsx';
 const ChessJS = require('chess.js');
 
 const ChessGame: React.FC = () => {
+  // State for the chess game logic
   const [game, setGame] = useState(new ChessJS.Chess());
   const [fen, setFen] = useState("start");
 
+  // State for the dynamic board size
+  const [boardSize, setBoardSize] = useState(800);
+
+  // Effect to update FEN when the game state changes
   useEffect(() => {
-    // Update the fen (Forsyth-Edwards Notation) whenever a move is made
     setFen(game.fen());
   }, [game]);
 
-const handleMove = (move: { from: string, to: string, promotion?: string }) => {
+  // Effect to handle window resize for responsive chessboard
+  useEffect(() => {
+    const updateBoardSize = () => {
+      // Adjust this calculation to change how the board size responds to window size
+      const newSize = Math.min(window.innerWidth, window.innerHeight) * 0.75;
+      setBoardSize(newSize);
+    };
+
+    window.addEventListener('resize', updateBoardSize);
+
+    // Initial resize
+    updateBoardSize();
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', updateBoardSize);
+  }, []);
+
+  // Function to handle move logic
+  const handleMove = (move: { from: string, to: string, promotion?: string }) => {
     try {
       let gameCopy = new ChessJS.Chess(game.fen());
       const legalMove = gameCopy.move({
         from: move.from,
         to: move.to,
-        promotion: move.promotion || "q" //Default return queen when pawn makes it
+        promotion: move.promotion || "q" // Default to queen promotion
       });
 
       if (legalMove) {
         setGame(gameCopy);
       } else {
         console.warn("Illegal move", move);
-        // The piece will automatically go back to its original position because we're not updating the state
       }
     } catch (error) {
-
       console.error("An error occurred", error);
     }
   };
 
-
-return (
+  // Render the chessboard with responsive width
+  return (
     <div>
       <Chessboard
-        width={800}
+        width={boardSize}
         position={fen}
         onDrop={({ sourceSquare, targetSquare }) => {
           handleMove({
